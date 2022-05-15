d775fa6b727c1526608a63354aff1355f70984fe
Efrain <efrain.pyauri@gmail.com>
2022-05-15 14:38:07 -0500

    Initial commit

---
+++ MathGame/app/src/main/java/com/eachmania/mathgame/Testing.java
@@ -0,0 +1,19 @@
+package com.eachmania.mathgame;
+
+
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function0;
+
+public class Testing {
+
+    public void testingSAM(@Nullable View.OnClickListener l){
+        throw new RuntimeException("Stub!");
+
+    }
+
+
+}
